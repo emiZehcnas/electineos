d9b1cb9a1971793ffa5d306c60c25fa70a3b67dd
emiZehcnas <emi.zehcnas@gmail.com>
2021-01-05 20:44:58 +0000

    optimization

--- api.py
+++ api.py
@@ -4,14 +4,18 @@ import asyncclick as click
 import mariadb
 import json
 import datetime
+import logging
 from kasa import SmartPlug
 from kasa import SmartDevice
 from pprint import pformat as pf
 from flask import Flask, request
 from datetime import datetime
+from datetime import date
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+logging.basicConfig(filename=str(date.today())+'.log', level=logging.INFO)
 
 config = {
 	'host': '127.0.0.1',
@@ -26,9 +30,10 @@ def connDB():
     try:
         conn = mariadb.connect(**config)
         cur = conn.cursor()
+        logging.info('Connexion à la base de données')
         return True
     except mariadb.Error as e:
-        stateConn = "Error: {e}"
+        logging.error('Echec lors de la connexion à la base de données : {e}')
         return False
         
 
@@ -41,10 +46,10 @@ def connSmart(host):
        
        plug = SmartPlug(host)
        asyncio.run(plug.update())
-       print('okkkkk')
+       logging.info('Récupération de la configuration de l\'équipement')
        return True
     except:
-       print('kooooo')
+       logging.error('Echec lors de la récupération de la configuration de l\'équipement')
        return False
        
        
@@ -52,7 +57,7 @@ def connSmart(host):
 
 def updateDeviceByHost(host):
     stateUpdate=""
-    if conn is True:
+    if connDB() is True:
        if connSmart(host) is True:
            #Get the state of plug : On/Off
            if (plug.is_off):
@@ -124,15 +129,14 @@ def getAlias():
 
 
 
-@app.route('/addDevice', methods=['GET','POST'])
-def addDevice():
+@app.route('/device/add', methods=['GET','POST'])
+def deviceAdd():
     #Initialization of variables
-    stateInsert=""
-    host = request.args.get('host')
-    conn = connDB()
+    status=""
+    host = request.args.get('host') 
     print(cur)
     #Connection Test
-    if conn is True:
+    if connDB() is True:
        #Connection to SmartDevice and SmartPlug
        if connSmart(host) is True:
            emeter_status = dev.emeter_realtime
@@ -147,26 +151,33 @@ def addDevice():
            cur.execute("SELECT host FROM devices WHERE host = '{}'".format(host)) 
            res = cur.fetchone() 
            if str(res) != "None": 
-              stateInsert = "L'équipement existe déjà"
+              status = "L'équipement existe déjà"
            else:
-              rqt_insertDevice = "INSERT INTO ELECTINEOS.devices (alias,model,host,hardware,mac,led_state,plug,statut,created_at,updated_at) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')"
-                  
-              cur.execute(rqt_insertDevice.format(dev.alias,dev.model,dev.host,dev.hw_info['hw_ver'],dev.mac,plug.led,plugState,'Plug disponible',datetime.now(),datetime.now()))
-              stateInsert = "L'insertion de l'équipement a été effectuée avec succès !"
+              try:
+                  rqt = "INSERT INTO ELECTINEOS.devices (alias,model,host,hardware,mac,led_state,plug,statut,created_at,updated_at) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')"    
+                  cur.execute(rqt.format(dev.alias,dev.model,dev.host,dev.hw_info['hw_ver'],dev.mac,plug.led,plugState,'Plug disponible',datetime.now(),datetime.now()))
+                  status = "L'insertion de l'équipement a été effectuée avec succès !"
+                  logging.info('deviceAdd : L\'insertion de l\'équipement : '+ host)
+              except:
+                  status = "Erreur lors de l'insertion de l'équipement"
+                  logging.error('deviceAdd : Erreur lors de l\'insertion de l\'équipement : '+ host)
        else:
-           stateInsert ="L'équipement est introuvable"
+           status ="L'équipement est introuvable"
+           logging.warning('deviceAdd : L\'équipement est introuvable')
+    else:
+       status: "Impossible de se connecter à la base de données"
+       logging.warning('deviceAdd : Impossible de connecter à la base de données')
 
-    return stateInsert
+    return status
 
 
 
 
-@app.route('/removeDevice', methods=['GET','POST'])
-def removeDevice():
-    stateRemove=""
-    idDevice = request.args.get('id')
-    conn = connDB()
-    if conn is True:
+@app.route('/device/remove', methods=['GET','POST'])
+def deviceRemove():
+    status=""
+    idDevice = request.args.get('id') 
+    if connDB() is True:
         try:
            #Get Host from table device by id
            req_host = "SELECT host from devices WHERE id='{}'"
@@ -181,67 +192,50 @@ def removeDevice():
            #delete emeter row by host
            req = "DELETE FROM emeter WHERE host = '{}'"
            cur.execute(req.format(host))
-           stateRemove="l'équipement a bien été supprimé"
+           status="l'équipement a bien été supprimé"
+           logging.info('Suppression de l\'équipement')
         except:
-           stateRemove="Erreur lors de la suppression de l'équipement"
+           status="Erreur lors de la suppression de l'équipement"
+           logging.error('deviceRemove : Erreur lors de la suppression de l\'équipement')
+           
     else:
-        stateRemove="Impossible de se connecter à la base de données"
+        status="Impossible de se connecter à la base de données"
+        logging.warning('deviceRemove : Impossible de connecter à la base de données')
     
-    return stateRemove
+    return status
 
 
 
-@app.route('/updateDeviceById', methods=['GET','POST'])
-def updateDeviceById():
-    stateUpdate=""
+@app.route('/device/update', methods=['GET','POST'])
+def deviceUpdate():
+    status =""
     idDevice = request.args.get('id')
-    conn = connDB()
-    if conn is True:
+    if connDB() is True:
        try:
            #Get Host from table device by id
-           req_host = "SELECT host from devices WHERE id='{}'"
-           cur.execute(req_host.format(idDevice))
+           rqt = "SELECT host from devices WHERE id='{}'"
+           cur.execute(rqt.format(idDevice))
            field_name = [field[0] for field in cur.description]
            res = cur.fetchone()
            value = dict(zip(field_name, res))
            host = value['host']
+           status = updateDeviceByHost(host)
+           logging.info('deviceUpdate : Mise à jour de L\'équipement')
        except:
-           stateUpdate="Erreur lors de la récupération de l'adresse IP"
-       if connSmart(host) is True:
-           #Get the state of plug : On/Off
-           if (plug.is_off):
-              plugState = 'off'
-           elif(plug.is_on):
-              plugState = 'on'
-           try:
-              rqt_updateDevice = "UPDATE devices SET alias='{}', model='{}', hardware='{}', mac='{}', led_state='{}', plug = '{}', statut='{}',updated_at='{}' WHERE host='{}' "
-              cur.execute(rqt_updateDevice.format(dev.alias,dev.model,dev.hw_info['hw_ver'],dev.mac,plug.led,plugState,'équipement disponible',datetime.now(),host))
-                
-              stateUpdate = "L'équipement a bien été mis à jour"
-           except:
-              stateUpdate = "Erreur lors de la mise à jour de l'équipement"
-       else:
-           try:
-               rqt_updateDevice = "UPDATE devices set statut='{}',updated_at='{}' WHERE host='{}' "
-               cur.execute(rqt_updateDevice.format('équipement indisponible',datetime.now(),host))
-               statutUpdate = "Impossible de récupérer les données de l'équipement"
-           except:
-               stateUpdate = "Erreur lors de la mise à jour de l'équipement"
+           status ="Erreur lors de la mise à jour de l'équipement"
+           logging.error('deviceUpdate : Erreur lors de la mise à jour de l\'équipement')
     else:
-        statutUpdate = "Impossible de se connecter à la base de données"
-           
-        
+       status="Impossible de se connecter à la base de données"
+       logging.warning('deviceRemove : Impossible de connecter à la base de données')
+
+    return status
     
-    return stateUpdate
-    
 
 
 
-@app.route('/updateAllDevices', methods=['GET'])
-def updateAllDevices():
-    global conn
-    conn = connDB()
-    if conn is True:
+@app.route('/devices/update', methods=['GET'])
+def devicesUpdate():
+    if connDB() is True:
         req="SELECT * from devices"
         cur.execute (req.format())
         row_headers=[x[0] for x in cur.description]
@@ -249,17 +243,17 @@ def updateAllDevices():
         for result in rv:
             row = dict(zip(row_headers,result))
             print(updateDeviceByHost(row['host']))
-        return ("ok")
+        Status ="Tous les équipements ont été mis à jour dans la base de données"
+        return Status
            
                 
     
 
 
-@app.route('/getDeviceById',methods=['GET','POST'])
-def getDeviceById():
-    conn = connDB()
+@app.route('/device',methods=['GET'])
+def device():
     idDevice = request.args.get('id')
-    if conn is True:
+    if connDB() is True:
         try:
             req="SELECT * from devices WHERE id = {}"
             cur.execute (req.format(idDevice))
@@ -271,17 +265,15 @@ def getDeviceById():
                 
             return json.dumps(json_data,indent=4, sort_keys=True, default=str)
         except:                        
- 
             return "Erreur lors de la récupération des données de l'équipement"        
             
             
             
 
-@app.route('/getEmeterById', methods=['GET','POST'])
-def getEmeterById():
-    conn = connDB()
+@app.route('/emeter', methods=['GET','POST'])
+def emeter():
     idDevice = request.args.get('id')
-    if conn is True:
+    if connDB() is True:
         try:
              #req = "select devices.*, emeter.id, emeter.host, UNIX_TIMESTAMP(emeter.statement_date) as statement_date, emeter.emeter_current, emeter.emeter_voltage, emeter.emeter_power, emeter.emeter_total_concumption, emeter.emeter_today, emeter_month from devices INNER JOIN emeter on devices.host=emeter.host WHERE devices.id='{}' AND MONTH(emeter.statement_date) = MONTH(CURRENT_DATE()) ORDER BY emeter.statement_date DESC"
              req = "select devices.*, emeter.id, emeter.host, UNIX_TIMESTAMP(emeter.statement_date) as statement_date, emeter.emeter_current, emeter.emeter_voltage, emeter.emeter_power, emeter.emeter_total_concumption, emeter.emeter_today, emeter_month from devices INNER JOIN emeter on devices.host=emeter.host WHERE devices.id='{}' ORDER BY emeter.statement_date DESC"
@@ -299,12 +291,11 @@ def getEmeterById():
  
  
  
-@app.route('/getTotalEmeterById', methods=['GET','POST'])
-def getCurrentEmeter():
+@app.route('/emeter/total', methods=['GET','POST'])
+def emeterTotal():
     current=""
     idDevice = request.args.get('id')
-    conn = connDB()
-    if conn is True:
+    if connDB() is True:
         try:
            #Get Host from table device by id
            req_host = "SELECT host from devices WHERE id='{}'"
@@ -330,12 +321,11 @@ def getCurrentEmeter():
 
 
 
-@app.route('/lightSwitch',methods=['GET','POST'])
+@app.route('/lightSwitch',methods=['GET'])
 def lightSwitch():
     stateUpdate=""
     idDevice = request.args.get('id')
-    conn = connDB()
-    if conn is True:
+    if connDB() is True:
         #Get Host from table device by id
         req_host = "SELECT host from devices WHERE id='{}'"
         cur.execute(req_host.format(idDevice))
@@ -404,11 +394,10 @@ def test():
 
 
 @app.route('/devices', methods=['GET'])
-def getDevice():
+def devices():
     stateConn=""
     req = "SELECT id,alias,model,host,hardware,mac,led_state,plug,statut,created_at,updated_at FROM devices"
-    conn = connDB()
-    if conn is True:
+    if connDB() is True:
         if request.method == 'GET':
             cur.execute(req.format())
             row_headers=[x[0] for x in cur.description]
